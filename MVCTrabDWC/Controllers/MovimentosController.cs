dd8c54d4211f37e1fbcdc0c8c0947ab770e12f13
DavidCarvalhido <davidcarvalhido@hotmail.com>
2026-01-31 21:50:58 +0000

    add the option to see the logistic movements

---
+++ MVCTrabDWC/Controllers/MovimentosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCTrabDWC.Data;
+
+namespace MVCTrabDWC.Controllers
+{
+    public class MovimentosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MovimentosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movimentos
+        public async Task<IActionResult> Index()
+        {
+            var movimentos = await _context.RegistosMaterial
+                .Include(r => r.Obra)
+                    .ThenInclude(o => o.Cliente)
+                .Include(r => r.Material)
+                .AsNoTracking()
+                .OrderByDescending(r => r.DataHora)
+                .ToListAsync();
+
+            return View(movimentos);
+        }
+    }
+}
