642fc76ee1eb4d0ad7981645849b9100e37ff401
DavidCarvalhido <davidcarvalhido@hotmail.com>
2026-02-01 21:12:21 +0000

    implementation of some metrics on the dashboard page

---
+++ MVCTrabDWC/Models/MovimentoStockResumoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTrabDWC.Models
+{
+    public class MovimentoStockResumoViewModel
+    {
+        public string Obra { get; set; }
+        public string Cliente { get; set; }
+        public string Operacao { get; set; } // ADD / REMOVE
+        public int Quantidade { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
